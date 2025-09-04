f779da73b338d3fb7d28074e4803d8955f9f786e
ryanflavor <ryanflavor@163.com>
2025-09-04 09:30:43 +0800

    chore: Remove test file

--- test_precommit.py
+++
@@ -1,8 +0,0 @@
-"""Test file to demonstrate pre-commit behavior."""
-
-
-def broken_function():
-    """This function has an obvious error."""
-    text = "hello"
-    text.append(" world")  # Error: str has no append method
-    return text
